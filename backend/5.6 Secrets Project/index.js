70d741c56ec4eecd523e81b3396a343061edbb4b
MadTown86 <grover.donlon@gmail.com>
2024-03-16 15:24:47 -0500

    Completed that project. Secrets with axios.

--- backend/5.6 Secrets Project/index.js
+++ backend/5.6 Secrets Project/index.js
@@ -1,3 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+
+const app = express();
+const port = 3000;
+
+const API_URL = "https://secrets-api.appbrewery.com";
+const yourBearerToken = "6d4c2050-9c2b-4a25-9a27-4703047d0ac9";
+
+app.use(express.static(__dirname + "\\public\\"));
+
+app.get("/", async (req, res) => {
+    try {
+        let secret = await axios.get(API_URL + "/random", {
+            headers: {
+                Authorization: `Bearer ${yourBearerToken}`
+        }})
+        res.render("index.ejs", {secret: secret.data.secret, user: secret.data.username});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 // HINTS:
 // 1. Import express and axios
 

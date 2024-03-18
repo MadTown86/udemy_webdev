7e24c271972f627354d499f6fcf056fcdc433866
MadTown86 <grover.donlon@gmail.com>
2024-03-18 11:30:37 -0500

    Not much changed.

--- backend/5.6 Secrets Project/index.js
+++ backend/5.6 Secrets Project/index.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+
+
+
 // HINTS:
 // 1. Import express and axios
 
